96e68de96a294341f6cd2c44cec656ff8c1534d7
Akarsh Satija <akarsh.satija@gmail.com>
2015-03-18 14:46:49 +0530

    responsive doen

--- index.php
+++ index.php
@@ -12,248 +12,111 @@
 
     <!-- Semantic META -->
     <meta name="keywords" content="">
-    <meta name="description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk!
-Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update].
-Got any questions or feedback? We are at idea@solomosalsa.com">
+    <meta name="description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk! Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update]. Got any questions or feedback? We are at idea@solomosalsa.com">
 
     <!-- Facebook META -->
     <meta property="og:site_name" content="Solomo Media: Best Social Media Marketing Company in India">
     <meta property="og:title" content="Facebook Page Likes Monitor-Solomosalsa">
-    <meta property="og:description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk!
-Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update].
-Got any questions or feedback? We are at idea@solomosalsa.com">
+    <meta property="og:description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk! Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update]. Got any questions or feedback? We are at idea@solomosalsa.com">
     <meta property="og:url" content="http://www.solomosalsa.com/facebook-page-likes-monitor/">
     <meta property="og:image" content="http://www.solomosalsa.com/facebook-page-likes-monitor/assets/img/LikeUnlike_1200x627px_1.png">
     <meta property="og:type" content="website">
 
     <!-- Twitter META -->
     <meta name="twitter:title" content="Facebook Page Likes Monitor-Solomosalsa">
-    <meta name="twitter:description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk!
-Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update].
-Got any questions or feedback? We are at idea@solomosalsa.com">
+    <meta name="twitter:description" content="We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk! Read more about Facebook's announcement about removing non-active users from the likes count on their blog[Link=https://www.facebook.com/business/news/page-likes-update]. Got any questions or feedback? We are at idea@solomosalsa.com">
     <meta name="twitter:url" content="http://www.solomosalsa.com/facebook-page-likes-monitor">
     <meta name="twitter:image" content="http://www.solomosalsa.com/facebook-page-likes-monitor/assets/img/LikeUnlike_1200x627px_1.png">
 
     <!-- Fav Icon -->
     <link href="http://www.solomosalsa.com/assets/img/fav.png" rel="shortcut icon" type="image/x-icon">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-    <link href="assets/css/style.css" rel="stylesheet" id="bootstrap-css">
-   
-    <style type="text/css">
-        .dos {
-            background: rgba(0, 0, 0, 0.90);
-            padding: 0px 0px 0px 0px;
-            box-shadow: 0 2px 12px rgba(0, 0, 0, 0.3);
-            z-index: 99;
-        }
-        .navbar-nav {
-            margin: 0;
-            /*font-family: roboto-light;*/
-            font-size: 15px;
-        }
-        
-        
-        .dos .navbar-nav > li > a:hover, .navbar-nav .active {
-            color: #fdcf08;
-            transition: 0.4s;
-        }
-        .navbar-nav .active {
-            /*font-family: roboto-regl;
-*/            font-size: 15px;
-        }
-        .dos .navbar-nav > .active > a, .dos .navbar-nav > .active > a:hover {
-            color: #fdcf08;
-            background-color: transparent;
-            text-align: center;
-            transition: 0.4s;
-        }
-        .dos .navbar-toggle {
-            border-color: #17181D;
-        }
-        .dos .navbar-toggle:hover {
-            background-color: #17181D;
-            border-color: #17181D;
-            transition: 0.4s;
-        }
-        .navbar-inverse .navbar-nav >li>a {
-            color: white;
-            text-align: center;
-        }
-
-        button:hover .icon-bar.lev1, button:hover .icon-bar.lev2, button:hover .icon-bar.lev3{
-            background-color: #fdcf08;
-            transition: 0.4s;
-        } 
-
-        button:focus .icon-bar.lev1, button:focus .icon-bar.lev2, button:focus .icon-bar.lev3{
-            background-color: #fdcf08;
-            transition: 0.4s;
-        } 
-
-        .navbar-inverse .navbar-toggle:hover, .navbar-inverse .navbar-toggle:focus {
-            background-color: #17181D!important;
-            transition: 0.4s;
-            /*prevents from focus to change on icon-bar*/
-        }
-        .change-toggle {
-            float: left;
-        }
-        /*navbar ends********************************************************/
-
-        .resp_img_fix {
-            width: 100%;
-        }
-        .contact_button {
-            display: inline-block;
-            margin-top: -41px;
-            padding: 5px;
-            margin-right: 10px;
-            border: 3px solid #fdcf08;
-            background-color: #17181D;
-            color: white;
-            width: 95px;
-        }
-        .contact_button:hover {
-            background-color: #fdcf08;
-            color: black;
-            transition: 0.4s;
-        }
-        .navbar-nav .contact_hide:hover {
-          background-color: #ffd544!important;
-          color: black!important;
-          cursor: pointer;
-          transition: 0.4s;
-        }
-    </style>
+    <link href="assets/css/style.css" rel="stylesheet" id="bootstrap-css">  
 </head>
 
 <body>
 <div id="navbar" class="navbar navbar-inverse navbar-fixed-top dos" role="banner" style="top: 0px;">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle change-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-                <span class="icon-bar lev1"></span>
-                <span class="icon-bar lev2"></span>
-                <span class="icon-bar lev3"></span>
-            </button>
-            <a href="http://www.solomosalsa.com/" class="navbar-brand">
-                <img class="logo" src="http://www.solomosalsa.com/assets/img/SOLOMO_LOGO.svg" alt="solomo logo">
-            </a><h1 class="pull-left title">Facebook Page Likes Monitor</h1>
-            <!-- <button type="button" class="contact_button pull-right" data-toggle="modal" data-target="#myModal">Contact</button> -->
-            <div class="navbar-collapse collapse navbar-responsive-collapse ">
-                <ul class="nav navbar-nav navbar-right selectors">
-                    <li><a href="http://www.solomosalsa.com/about" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-about']);">About</a>
-                    </li>
-                    <li><a href="http://www.solomosalsa.com/services" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-services']);">Services</a>
-                    </li>
-                    <li><a href="http://www.solomosalsa.com/clients" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-clients']);">Clients</a>
-                    </li>
-                    <li><a href="http://www.solomosalsa.com/careers" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-careers']);">Careers</a>
-                    </li>
-                    <li><a href="http://www.solomosalsa.com/blog" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-blog']);">Blog</a>
-                    </li>
-                    <li><a href="http://www.solomosalsa.com/#myModal" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-contact']);">Contact</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle change-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+            <span class="icon-bar lev1"></span>
+            <span class="icon-bar lev2"></span>
+            <span class="icon-bar lev3"></span>
+        </button>
+        <a href="http://www.solomosalsa.com/" class="navbar-brand">
+            <img class="logo" src="http://www.solomosalsa.com/assets/img/SOLOMO_LOGO.svg" alt="solomo logo">
+        </a>
+        <h1 class="pull-left title">Facebook Page Likes Monitor</h1>
+        <!-- <button type="button" class="contact_button pull-right" data-toggle="modal" data-target="#myModal">Contact</button> -->
+        <div class="navbar-collapse collapse navbar-responsive-collapse ">
+            <ul class="nav navbar-nav navbar-right selectors">
+                <li><a href="http://www.solomosalsa.com/about" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-about']);">About</a>
+                </li>
+                <li><a href="http://www.solomosalsa.com/services" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-services']);">Services</a>
+                </li>
+                <li><a href="http://www.solomosalsa.com/clients" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-clients']);">Clients</a>
+                </li>
+                <li><a href="http://www.solomosalsa.com/careers" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-careers']);">Careers</a>
+                </li>
+                <li><a href="http://www.solomosalsa.com/blog" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-blog']);">Blog</a>
+                </li>
+                <li><a href="http://www.solomosalsa.com/#myModal" onclick="_gaq.push(['_trackEvent', 'solomo-website', 'navbar-click', 'tab-contact']);">Contact</a>
+                </li>
+            </ul>
         </div>
-        <div class="pull-left title-description"><p>We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk!
-Read more about Facebook's announcement about removing non-active users from the likes count on their <a target="_blank" href=" https://www.facebook.com/business/news/page-likes-update">blog</a>.
-Got any questions or feedback? We are at <a href="mailto:idea@solomosalsa.com">idea@solomosalsa.com</a></p></div>
-        <div id="navbar" class="filter-category pull-right">
-                <div class="bs-example" data-example-ids="select-form-control">
-                    <form id="category_form">
-                      <select class="form-control category-filter" name="category" onchange="submit()">Celebrity
-                     </select>
-                    </form>
-                  </div>
-            </div>
     </div>
-<!-- -->
+</div>
 
-<?php /*
-    <nav class="navbar navbar-fixed-top navbar-inverse top-nav-bar">
-        <div class="container">
-            <div class="navbar-header">
-                
-                <a class="navbar-brand" href="#"><img src="http://www.solomosalsa.com/assets/img/SOLOMO_LOGO.svg">
-                </a>
-            <div id="navbar" class="filter-category pull-right">
-                <div class="bs-example" data-example-ids="select-form-control">
-                    <form id="category_form">
-                      <select class="form-control category-filter" name="category" onchange="submit()">Celebrity
-                     </select>
-                    </form>
-                  </div>
-            </div>
 
-            </div>
-            
-            <!-- /.nav-collapse -->
+<div class="container content">
+<div class="description-box col-md-12">
+    <div class="pull-left title-description"><p>We bring you the Facebook Pages Likes Monitor in our crusade against fake likes and the ilk!
+        Read more about Facebook's announcement about removing non-active users from the likes count on their <a target="_blank" href=" https://www.facebook.com/business/news/page-likes-update">blog</a>.
+        Got any questions or feedback? We are at <a href="mailto:idea@solomosalsa.com">idea@solomosalsa.com</a></p>
+    </div>
+    <div id="navbar" class="filter-category pull-right">
+        <div class="bs-example" data-example-ids="select-form-control">
+            <form id="category_form">
+              <select class="form-control category-filter" name="category" onchange="submit()">Celebrity
+             </select>
+            </form>
         </div>
-        <!-- /.container -->
-    </nav>
-    <!-- /.navbar -->
-*/?>
-
-    <div class="container content">
-    <div id="loader" style="width:800px; margin:0 auto;">
+    </div>
+</div>
+    <div id="loader" style="width:100%; margin:0 auto;">
        <!-- <img src="indicator.gif"/> -->
-     </div>
-        <div class="row">
-            
-            <div id="no-more-tables">
-                <table class="col-md-12 col-bg-12 col-sm-12 table-condensed cf fblikes-table">
- <!--                    <thead class="cf">
-                        <tr>
-                            <th class="menu-right">Date </th>
-                            <th class="numeric">11 Mar '15</th>
-                            <th class="numeric">12 Mar '15</th>
-                            <th class="numeric">13 Mar '15</th>
-                            <th class="numeric">14 Mar '15</th>
-                            <th class="numeric">15 Mar '15</th>
-                            <th class="numeric">16 Mar '15</th>
-                            <th class="numeric">%age Change </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-
-                    </tbody> -->
-                </table>
-            </div>
+    </div>
+    <div class="row"> 
+        <div id="no-more-tables">
+            <table class="col-md-12 col-bg-12 col-sm-12 table-condensed cf fblikes-table">
+            </table>
         </div>
     </div>
+</div>
 <div class="footer">
-            <p class="text-center">© 2014-2015 Solomo Media Pvt. Ltd. All rights reserved.</p>
-        </div>
+    <p class="text-center">© 2014-2015 Solomo Media Pvt. Ltd. All rights reserved.</p>
+</div>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-42880987-1', 'solomosalsa.com');
+ga('send', 'pageview');
+
+//GA for event tracking
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-42880987-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>  
+
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="assets/js/script.js"></script>
+<script src="assets/js/stupidtable.min.js"></script>
 </body>
-    <script>
-           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-           })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-           ga('create', 'UA-42880987-1', 'solomosalsa.com');
-           ga('send', 'pageview');
-       </script>
-
-    <!-- event tracking code below-->
-    <script type="text/javascript">
-    //GA for event tracking
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-42880987-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-    </script>  
-    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <script src="assets/js/script.js"></script>
-    <script src="assets/js/stupidtable.min.js"></script>
-
 </html>
